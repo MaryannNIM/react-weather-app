94776f363c63827f59673e35526f895ffc3c7e16
Maryann <Onunakum@gmail.com>
2023-07-15 17:41:25 +0200

    Created the structure of the project

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <weather />
+        <footer>
+          This project was coded by Maryann Nzekwe and it is{" "}
+          <a
+            href="https://github.com/MaryannNIM/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on github
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
