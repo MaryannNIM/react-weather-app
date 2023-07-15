e8da75a1ecc242b0b86ce29a0b21908bd1cd521e
Maryann <Onunakum@gmail.com>
2023-07-15 21:23:43 +0200

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
-import Weather from "./Weather.js";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
-        <weather />
+        <Weather />
         <footer>
           This project was coded by Maryann Nzekwe and it is{" "}
           <a
